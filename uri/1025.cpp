e4858fc723f696778724f579fdc573ba460b7f58
Beloin Rodrigues <beloinrodrigues.lds@grupomulti.com.br>
2024-01-30 16:56:36 -0300

    WIP

---
+++ uri/1025.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include "bits/stdc++.h"
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    unsigned int Q, N;
+
+    string s;
+    cin >> s;
+    string delimiter = " ";
+    string token = s.substr(0, s.find(delimiter));
+//    while (Q != 0 || N != 0) {
+//    }
+
+    return 0;
+}
